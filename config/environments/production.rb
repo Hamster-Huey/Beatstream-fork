4af544789b9f7bdb8dc3fe75412a4c5e44f4b6ec
unknown <jamchri@MICROSO-VV7BHUM.redmond.corp.microsoft.com>
2017-05-04 16:39:33 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Beatstream::Application.configure do
-  config.cache_classes = false
+  config.cache_classes = true
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
   config.serve_static_assets = true
 
   config.assets.compress = true
